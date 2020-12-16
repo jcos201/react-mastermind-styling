6cb5c0d05deb829c0f8c1993b87accbcf7677a53
Jorge L. Cossi <jcos201@gmail.com>
2020-12-16 16:18:33 -0500

    react mastermind styling deliverable commit

---
+++ src/components/GuessPeg/GuessPeg.js
@@ -0,0 +1,10 @@
+import styles from './GuessPeg.module.css'
+
+const GuessPeg = (props) => (
+  <div 
+    className={styles.Peg} 
+    style={{backgroundColor: props.color}}
+  />
+);
+
+export default GuessPeg;
